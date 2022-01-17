13e4c786b337c464013b4429cd6ddcfd4fa5fd41
Hugo Lapa <Hugolapa888@gmail.com>
2022-01-17 11:43:43 +0000

    manifest + js script fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Gets the body element 
+//Gets an array of body element 
 let background = document.getElementsByTagName('body');
 
 //adds css so the background becomes red
-background.style['background-color'] = 'red';
+background[0].style['background-color'] = 'red';
