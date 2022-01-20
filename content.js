0ffa36aedac62fd1df8b8b537bb3333acad1a02a
Hugo Lapa <Hugolapa888@gmail.com>
2022-01-20 09:43:20 +0000

    background color changes

--- content.js
+++ content.js
@@ -1,9 +1,17 @@
 console.log("chrom extensioin is aago");
+
+let color = "red";
+
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(msg, sender, sendResponse) {
     console.log(msg);
     if (msg.command == "blue") {
         console.log("itss blueeee!!");
+        color = "blue";
     }
 }
+
+
+let body = document.getElementsByTagName('body');
+body[0].style['background-color'] = color;
